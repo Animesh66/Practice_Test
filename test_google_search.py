131709ea1fa54c1707503c787569f7a2604dd8a7
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-09 15:33:23 +0530

    reformatting

--- test_google_search.py
+++ test_google_search.py
@@ -13,9 +13,10 @@ def test_solution():
     driver.find_element(By.XPATH, "//input[@title='Search']").send_keys("Selenium with Python")
     driver.find_element(By.XPATH, "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']").click()
     search_links = driver.find_elements(By.XPATH, "//h3[@class='LC20lb DKV0Md']")
+    # search_links[0].click()
     link = search_links[1]
-    driver.execute_script("arguments[0].scrollIntoView();", link)
+    driver.execute_script("arguments[0].scrollIntoView();", link) # scroll down to the element
+    time.sleep(3)
     link.click()
-    time.sleep(5)
     assert "Selenium with Python" in driver.page_source, "Given search text is present in the website."
     driver.quit()
